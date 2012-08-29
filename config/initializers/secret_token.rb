7eda0d602ca9e0b0f7d19b3a994aa2bb8c2665f5
Christian Clough <christian.clough@gmail.com>
2012-08-29 13:31:51 +0100

    initial rails app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Casenexus::Application.config.secret_token = 'ccebcf1af2cffba446827b8d216dbeed9856b10c7aa0c9c2f1bd09c3ae438ead55b2d5acbc8536bcbddab4c646c4bd2646f0433ef2c48a8677613ba4259dce43'
