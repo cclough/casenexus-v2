e9c04410f12da460a2526cd9ae46a3bb9ae77942
Christian Clough <christian.clough@gmail.com>
2012-09-05 23:50:31 +0100

    library

--- app/assets/javascripts/cases.js
+++ app/assets/javascripts/cases.js
@@ -2,58 +2,6 @@
 // All this logic will automatically be available in application.js.
 
 
-/////////////////////////////////////////////////////////////////
-///////////////////////////// SHOW //////////////////////////////
-/////////////////////////////////////////////////////////////////
-
-
-  var chart;
-
-  // var chartData is set in view
-
-  AmCharts.ready(function () {
-    // RADAR CHART
-    chart = new AmCharts.AmRadarChart();
-    chart.dataProvider = chartData_show_radar;
-    chart.categoryField = "criteria";
-    chart.startDuration = 1;
-    chart.startEffect = ">";
-    chart.sequencedAnimation = true;
-
-    // VALUE AXIS
-    var valueAxis = new AmCharts.ValueAxis();
-    valueAxis.gridType = "circles";
-    valueAxis.fillAlpha = 0.02;
-    valueAxis.fillColor = "#000000"
-    valueAxis.axisAlpha = 0.1;
-    valueAxis.gridAlpha = 0.1;
-    valueAxis.fontWeight = "bold"
-    valueAxis.minimum = 0;
-    valueAxis.maximum = 10;
-    chart.addValueAxis(valueAxis);
-
-    // GRAPH
-    var graph = new AmCharts.AmGraph();
-    graph.lineColor = "#98cdff"
-    graph.fillAlphas = 0.4;
-    graph.bullet = "round"
-    graph.valueField = "score"
-    graph.balloonText = "[[category]]: [[value]]/10"
-    chart.addGraph(graph);
-
-    // Balloon Settings
-    var balloon = chart.balloon;
-    balloon.adjustBorderColor = true;
-    balloon.color = "#000000";
-    balloon.cornerRadius = 5;
-    balloon.fillColor = "#000000";
-    balloon.fillAlpha = 0.7
-    balloon.color = "#FFFFFF"
-
-    // WRITE
-    chart.write("chartdiv");
-  });
-
 
 /////////////////////////////////////////////////////////////////
 ///////////////////////// ANALYSIS //////////////////////////////
@@ -220,6 +168,12 @@
     graph.valueField = "structure";
     graph.lineAlpha = 1;
     graph.fillAlphas = 0.6; // setting fillAlphas to > 0 value makes it area graph
+    graph.bullet = "round"
+    graph.addListener("clickGraphItem", function (event) {
+      // do whatever you need to do when clicked
+      // ...
+    });
+    
     chart.addGraph(graph);
 
     // second graph - ANALYTIC
@@ -313,4 +267,61 @@
       // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
       chart.zoomToIndexes(chartData_progress.length - 40, chartData_progress.length - 1);
   }
+
+
+  function amRolledOverBullet(chart_id, graph_index, value, series, url, description){
+    alert('Rolled over a bullet ' + series);
+  }
+
+/////////////////////////////////////////////////////////////////
+///////////////////////////// SHOW //////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+
+  var chart;
+
+  // var chartData is set in view
+
+  AmCharts.ready(function () {
+    // RADAR CHART
+    chart = new AmCharts.AmRadarChart();
+    chart.dataProvider = chartData_show_radar;
+    chart.categoryField = "criteria";
+    chart.startDuration = 1;
+    chart.startEffect = ">";
+    chart.sequencedAnimation = true;
+
+    // VALUE AXIS
+    var valueAxis = new AmCharts.ValueAxis();
+    valueAxis.gridType = "circles";
+    valueAxis.fillAlpha = 0.02;
+    valueAxis.fillColor = "#000000"
+    valueAxis.axisAlpha = 0.1;
+    valueAxis.gridAlpha = 0.1;
+    valueAxis.fontWeight = "bold"
+    valueAxis.minimum = 0;
+    valueAxis.maximum = 10;
+    chart.addValueAxis(valueAxis);
+
+    // GRAPH
+    var graph = new AmCharts.AmGraph();
+    graph.lineColor = "#98cdff"
+    graph.fillAlphas = 0.4;
+    graph.bullet = "round"
+    graph.valueField = "score"
+    graph.balloonText = "[[category]]: [[value]]/10"
+    chart.addGraph(graph);
+
+    // Balloon Settings
+    var balloon = chart.balloon;
+    balloon.adjustBorderColor = true;
+    balloon.color = "#000000";
+    balloon.cornerRadius = 5;
+    balloon.fillColor = "#000000";
+    balloon.fillAlpha = 0.7
+    balloon.color = "#FFFFFF"
+
+    // WRITE
+    chart.write("chartdiv");
+  });
 
