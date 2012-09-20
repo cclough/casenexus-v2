02fc0199264a575d0cca77f126956989682aec66
Christian Clough <christian.clough@gmail.com>
2012-09-20 23:13:32 +0100

    unlink email observer

--- app/models/notification_observer.rb
+++ app/models/notification_observer.rb
@@ -1,43 +1,43 @@
 class NotificationObserver < ActiveRecord::Observer
 
 
-  def after_create(notification)
+  # def after_create(notification)
 
-    # NB .content has varying roles
-    user_from = User.find_by_id(notification.sender_id)
+  #   # NB .content has varying roles
+  #   user_from = User.find_by_id(notification.sender_id)
 
-    case notification.ntype
-    when "welcome"
-      UserMailer.welcome(notification.target, 
-                         notification.url).deliver
-    when "feedback"
-      UserMailer.feedback(user_from,
-                          notification.target, 
-                          notification.url, 
-                          notification.event_date,
-                          notification.content).deliver
-    when "feedback_req"
-      UserMailer.feedback_req(user_from,
-                              notification.target, 
-                              notification.url,
-                              notification.event_date,
-                              notification.content).deliver
-    when "message"
-      UserMailer.usermessage(user_from,
-                             notification.target, 
-                             notification.url, 
-                             notification.content).deliver
-    when "friendship_req"
-      UserMailer.friendship_req(user_from,
-                                notification.target,
-                                notification.url,
-                                notification.content).deliver
-    when "friendship_app"
-      UserMailer.friendship_app(user_from,
-                                notification.target, 
-                                notification.url).deliver
-    end
+  #   case notification.ntype
+  #   when "welcome"
+  #     UserMailer.welcome(notification.target, 
+  #                        notification.url).deliver
+  #   when "feedback"
+  #     UserMailer.feedback(user_from,
+  #                         notification.target, 
+  #                         notification.url, 
+  #                         notification.event_date,
+  #                         notification.content).deliver
+  #   when "feedback_req"
+  #     UserMailer.feedback_req(user_from,
+  #                             notification.target, 
+  #                             notification.url,
+  #                             notification.event_date,
+  #                             notification.content).deliver
+  #   when "message"
+  #     UserMailer.usermessage(user_from,
+  #                            notification.target, 
+  #                            notification.url, 
+  #                            notification.content).deliver
+  #   when "friendship_req"
+  #     UserMailer.friendship_req(user_from,
+  #                               notification.target,
+  #                               notification.url,
+  #                               notification.content).deliver
+  #   when "friendship_app"
+  #     UserMailer.friendship_app(user_from,
+  #                               notification.target, 
+  #                               notification.url).deliver
+  #   end
   
-  end
+  # end
 
 end
