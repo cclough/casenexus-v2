b6ce56f6284e95647a8daa13b1b8fcbbea17c927
Christian Clough <christian.clough@gmail.com>
2012-09-21 00:47:51 +0100

    haml roulette2 index

--- config/initializers/set_datestyle.rb
+++ config/initializers/set_datestyle.rb
@@ -1,1 +1,2 @@
+# Needed for heroku's postgres - otherwise doesn't understand date format in sample_data
 ActiveRecord::Base.connection.execute 'SET DATESTYLE TO PostgreSQL,European;'
---
+++ db/migrate/20120920233617_add_roulette_tables.rb
@@ -0,0 +1,7 @@
+class AddRouletteTables < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
