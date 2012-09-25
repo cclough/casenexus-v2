db87207234fd920297278fccb30a954b20556c3d
Christian Clough <christian.clough@gmail.com>
2012-09-25 13:48:58 +0100

    tweak roulette

--- app/assets/javascripts/roulette.js
+++ app/assets/javascripts/roulette.js
@@ -1,34 +1,37 @@
+$(document).ready(function(){
 
-$("#roulette_index_button_connect").click(function() {
-  
-  var socket = io.connect('https://cclough.nodejitsu.com', {secure: true});
+  $("#roulette_index_button_connect").click(function() {
 
-  // on connection to server, ask for user's name with an anonymous callback
-  socket.on('connect', function(){
-  	// call the server-side function 'adduser' and send one parameter (value of prompt)
-  	socket.emit('adduser', '#{rand(100)}');
-  });
+    var socket = io.connect('https://cclough.nodejitsu.com', {secure: true});
 
-  // listener, whenever the server emits 'updatechat', this updates the chat body
-  socket.on('updatelog', function (username, data) {
-  	$('#roulette_index_log').append('<div class=roulette_index_log_item>'+username + ': ' + data + '</div>');
-  });
+    // on connection to server, ask for user's name with an anonymous callback
+    socket.on('connect', function(){
+    	// call the server-side function 'adduser' and send one parameter (value of prompt)
+    	socket.emit('adduser', roulette_index_user_name);
+    });
 
-  // listener, whenever the server emits 'updateusers', this updates the username list
-  socket.on('updateusers', function(data) {
-  	$('#roulette_index_users').empty();
-  	$.each(data, function(key, value) {
+    // listener, whenever the server emits 'updatechat', this updates the chat body
+    socket.on('updatelog', function (username, data) {
+    	$('#roulette_index_log').append('<div class=roulette_index_log_item>'+username + ': ' + data + '</div>');
+    });
 
-      $.get('/get_item?id=' + key, function(data) {
+    // listener, whenever the server emits 'updateusers', this updates the username list
+    socket.on('updateusers', function(data) {
+    	$('#roulette_index_users').empty();
+    	$.each(data, function(key, value) {
 
-        $('#roulette_index_users').append('<div class=roulette_index_users_item id=roulette_index_users_item_'+key+'>' + data + '</div>');
-        
-        $('#roulette_index_users_item_' + key).fadeIn('fast');
+        $.get('/get_item?id=' + key, function(data) {
 
-      });
+          $('#roulette_index_users').append('<div class=roulette_index_users_item id=roulette_index_users_item_'+key+'>' + data + '</div>');
+          
+          $('#roulette_index_users_item_' + key).fadeIn('fast');
 
-  	});
-    
-  });
+        });
 
-)};
+    	});
+      
+    });
+
+  )};
+
+});
