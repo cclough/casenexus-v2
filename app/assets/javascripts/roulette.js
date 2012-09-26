27f1c51e88e930962ea5202b6ea35ebae9c4aa64
Christian Clough <christian.clough@gmail.com>
2012-09-26 03:08:17 +0100

    more rouletteee

--- app/assets/javascripts/roulette.js
+++ app/assets/javascripts/roulette.js
@@ -58,6 +58,8 @@ $(document).ready(function(){
 
         if ($.inArray(key_local, roulette_index_users_remote) === -1) {
 
+          $("#debug_localnotinremote").append(key_local);
+
           var idx = roulette_index_users_local.indexOf(key_local); // Find the index
 
           if(idx!=-1) {
