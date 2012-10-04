08b8843d7c6e38d92665a1444cdfd55b90c3d97b
Christian Clough <christian.clough@gmail.com>
2012-10-05 00:07:25 +0100

    geo code start

--- app/models/user.rb
+++ app/models/user.rb
@@ -99,14 +99,14 @@ class User < ActiveRecord::Base
   geocoded_by :ip_address,
     :latitude => :lat, :longitude => :lng
 
-  reverse_geocoded_by :lat, :lng do |obj,results|
-    if geo = results.first
-      obj.city    = geo.city
-      obj.country = geo.country_code
-    end
-  end
+  # reverse_geocoded_by :lat, :lng do |obj,results|
+  #   if geo = results.first
+  #     obj.city    = geo.city
+  #     obj.country = geo.country_code
+  #   end
+  # end
 
-  after_validation :geocode, :reverse_geocode
+  after_validation :geocode, #:reverse_geocode
 
 
 
