4a21df3a5778ade01285669640e2c16dbe7896dc
Christian Clough <christian.clough@gmail.com>
2012-10-04 03:00:41 +0100

    home page arrownav and start help modal

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
 	$("select, input:checkbox").uniform();
 
 	$('input, textarea').placeholder();
+
+
+  $('#modal_help').modal({
+    backdrop: false,
+    show: false
+  });
+
+  $('#header_link_help').click(function() {
+    $('.modal').modal('show');
+  });
+
 });
--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,14 +1,41 @@
-$(document).ready(function(){
+// Main Navigation
+var ArrowNav = {
+  init: function() {
+    $("a[href*=#]").click(function(e) {
 
-  $("#static_home_slider").royalSlider({
-      // options go here
-      // as an example, enable keyboard arrows nav
-      keyboardNavEnabled: true,
-    	autoPlay: {
-    		// autoplay options go gere
-    		enabled: true,
-    		pauseOnHover: true
-    	}
-  });
-	
-});
+      e.preventDefault();
+      if($(this).attr("href").split("#")[1]) {
+        ArrowNav.goTo($(this).attr("href").split("#")[1]);
+      }
+
+    });
+    this.goTo("1");
+  },
+  goTo: function(page) {
+    var next_page = $("#static_home_arrownav_"+page);
+    var nav_item = $('nav ul li a[href=#'+page+']');
+
+    $("nav ul li").removeClass("current");
+    nav_item.parent().addClass("current");
+
+    $(".arrownav_page").fadeOut(500, function() {
+      $(".arrownav_page").removeClass("current");
+      next_page.addClass("current");
+      next_page.fadeIn(500);
+    });
+
+    ArrowNav.centerArrow(nav_item);
+    
+  },
+  centerArrow: function(nav_item, animate) {
+    var left_margin = (nav_item.parent().position().left + nav_item.parent().width()) + 24 - (nav_item.parent().width() / 2);
+    if(animate != false) {
+      $("nav .arrow").animate({
+        left: left_margin - 8
+      }, 500, function() { $(this).show(); });
+    } else {
+      $("nav .arrow").css({ left: left_margin - 8 });
+    }
+  }
+
+};
