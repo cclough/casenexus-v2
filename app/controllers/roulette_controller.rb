0cde520279416110b7bf65655ac002b80e7c2e42
Christian Clough <christian.clough@gmail.com>
2012-10-10 19:47:54 +0100

    roulette comment out completed_user

--- app/controllers/roulette_controller.rb
+++ app/controllers/roulette_controller.rb
@@ -1,14 +1,14 @@
 class RouletteController < ApplicationController
 
   before_filter :signed_in_user, only: [:index]
-  before_filter :completed_user
+  # before_filter :completed_user
   
   def index
     current_user.update_attribute(:roulette_token, ('a'..'z').to_a.shuffle[0,8].join)
   end
 
   def item
-    @user = User.find(params[:id])
+    @roulette_user = User.find(params[:id])
     respond_to do |format|
       format.html { render :layout => false } 
     end
