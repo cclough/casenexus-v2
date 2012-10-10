c735be9e64f5c8b75004e8b2faeda17219660057
Christian Clough <christian.clough@gmail.com>
2012-10-10 23:25:10 +0100

    add chevrons

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,23 +1,36 @@
 module UsersHelper
 
-	def avatar_for(user)
+	def avatar_for(user, type)
 
-		# case user.casecount
-		# when 0..9 then avatar_colour = "grey"
-		# when 10..19 then avatar_colour = "blue"
-		# when 20..29 then avatar_colour = "yellow"
-		# when 30..49 then avatar_colour = "orange"
-		# when 50..1000 then avatar_colour = "purple"
-		# end
+		case type
+		when "icon"
 
-		avatar_colour = "orange"
-		
-		avatar_url = "avatars/avatar_" + avatar_colour + ".png"
+			# case user.casecount
+			# when 0..9 then avatar_colour = "grey"
+			# when 10..19 then avatar_colour = "blue"
+			# when 20..29 then avatar_colour = "yellow"
+			# when 30..49 then avatar_colour = "orange"
+			# when 50..1000 then avatar_colour = "purple"
+			# end
 
-		avatar_alt = user.name + " has done " + user.casecount.to_s + " cases"
+			avatar_colour = "orange"
+			
+			avatar_url = "avatars/avatar_" + avatar_colour + ".png"
 
-		image_tag(avatar_url, alt: avatar_alt)
-	
+			avatar_alt = user.name + " has done " + user.casecount.to_s + " cases"
+
+			image_tag(avatar_url, alt: avatar_alt)
+
+		when "chevron"
+
+			avatar_colour = "1"
+			
+			avatar_style = "background-image: url(/assets/chevrons/chevron_" + avatar_colour + ".png);"
+
+			content_tag :div, style: avatar_style, class: 'application_chevron' do
+				content_tag(:div, current_user.casecount) + content_tag(:div, 'cases', class: 'application_chevron_text')
+			end
+		end
 	end
 
 end
