ac30c4f6e225e37c30ec1f75a30bf0f56dbd5416
Christian Clough <christian.clough@gmail.com>
2012-10-18 23:14:46 +0100

    more roulette

--- app/assets/javascripts/roulette.js
+++ app/assets/javascripts/roulette.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
     // Request send (private message)
     $(".roulette_index_item_button_request").click(function() {
         
-        var target_user_id = $(self).attr('data-user_id');
+        var target_user_id = $(this).attr('data-user_id');
 
         socket.emit("private", { msg: "Request to skype", to: target_user_id });
 
