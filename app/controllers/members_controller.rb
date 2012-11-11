5a50726595761f07e516b305e1bcd0777615c336
Christian Clough <christian.clough@gmail.com>
2012-11-11 21:14:12 +0000

    member show radar data-from-model fix

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -33,7 +33,7 @@ class MembersController < ApplicationController
     @user = User.find(params[:id])
 
     # defines radar chart last 5 count
-    (current_user.case_count < 6) ? (@radar_count = 1) : (@radar_count = 5)
+    (current_user.cases.where("interviewer_id = #{@user.id}").count < 6) ? (@radar_count = 1) : (@radar_count = 5)
 
     respond_to do |format|
       @notification = @user.notifications.build
