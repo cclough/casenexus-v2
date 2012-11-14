cbafdacac3cd5af80567c35996b439cd600a1b56
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-11-14 17:51:22 -0300

    fixing country select

--- app/models/country.rb
+++ app/models/country.rb
@@ -3,7 +3,10 @@ class Country < ActiveRecord::Base
 
   class << self
     def in_use
-      User.where("length(country) > 0").order('country asc').select("country, city, lat, lng").uniq { |u| { country: u.country, city: u.city, lat: u.lat, lng: u.lng } }
+      users = User.where("length(country) > 0").order('country asc').select("country, city, lat, lng")
+      records = {}
+      users.each { |user| records["#{user.country}#{user.city}"] = user unless records["#{user.country}#{user.city}"] }
+      records.collect { |k, u| { country: u.country, city: u.city, lat: u.lat, lng: u.lng } }
     end
   end
 end
