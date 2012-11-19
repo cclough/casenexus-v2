e2d7f130746d3bcf1d16ffebf79198dd2ca615d7
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-11-19 00:57:14 -0300

    Status confirmed for user filter

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -8,11 +8,11 @@ class MembersController < ApplicationController
     # Set scope of users list depending on params from filter menu
     case params[:users_listtype]
       when "local"
-        users_scope = User.status_approved.includes(:cases).list_local(current_user)
+        users_scope = User.confirmed.includes(:cases).list_local(current_user)
       when "global"
-        users_scope = User.status_approved.includes(:cases).list_global
+        users_scope = User.confirmed.includes(:cases).list_global
       when "online"
-        users_scope = User.status_approved.includes(:cases).list_online
+        users_scope = User.confirmed.includes(:cases).list_online
       when "contacts"
         users_scope = current_user.accepted_friends.includes(:cases)
     end
