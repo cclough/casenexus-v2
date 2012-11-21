0d94255e201f59cd57ee4f078fd583c9712855d2
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-11-21 15:23:06 -0300

    User domain

--- app/models/user.rb
+++ app/models/user.rb
@@ -219,7 +219,7 @@ class User < ActiveRecord::Base
     if self.linkedin_uid.blank?
       begin
         domain = self.email.split("@")[1]
-        errors.add(:email, "not from listed University") unless University.where(domain: domain).exists?
+        errors.add(:email, "not from listed University") unless University.where("domain like ?", "%#{domain}").exists?
       rescue Exception => e
         errors.add(:email, "Invalid Email")
       end
