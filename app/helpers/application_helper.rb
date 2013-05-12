ee54d2ccc826b7a0469306ce37e7e959ac756ba3
Christian Clough <christian.clough@gmail.com>
2013-05-12 01:44:01 +0100

    remove chevrons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,12 +36,6 @@ module ApplicationHelper
         avatar_alt = user.name + " has done " + user.cases.count.to_s + " cases"
         image_tag(avatar_url, alt: avatar_alt, class: "application_avatar_icon", "data-original-title" => user.name + " has done " + user.cases.count.to_s + " cases", rel: "tooltip", "data-placement"=>"right")
 
-      when "chevron"
-        chevron_num = "1"
-        chevron_style = "background-image: url(/assets/chevrons/chevron_" + chevron_num + ".png);"
-        content_tag :div, style: chevron_style, class: 'application_chevron' do
-          content_tag(:div, user.cases.count.to_s) + content_tag(:div, 'cases', class: 'application_chevron_text')
-        end
     end
   end
 
