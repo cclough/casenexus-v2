645d7c4dd8470d5c1597c6944218f36a245115e4
Christian Clough <christian.clough@gmail.com>
2013-08-17 01:19:25 +0100

    event email finalise

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -78,18 +78,20 @@ class UserMailer < ActionMailer::Base
     @url = url
     @ntype = ntype
 
+    unless @event.book_id_usertoprepare.blank?
+      @book_usertoprepare = Book.find(@event.book_id_usertoprepare)
+    end
+
     # Attach selected case file, if selected
     unless (ntype == "event_cancel_partner") || @event.book_id_partnertoprepare.blank?
       @book_partnertoprepare = Book.find(@event.book_id_partnertoprepare)
-
-      # host = Rails.env == 'production' ? 'www.casenexus.com' : 'localhost:3000'
-      # url = Rails.application.routes.url_helpers.root_url(host: host)
       attachments["case.pdf"] = File.read(File.join(Rails.root, 'app','assets','images','library',@book_partnertoprepare.url))
     end
 
     email_with_name = "#{@user_target.name} <#{@user_target.email}>"
     mail(to: email_with_name, subject: "casenexus.com: " + title)
   end
+
 
   def event_setchangecancelremind_sender(user_target, event_id, title, url, ntype)
     @user_target = user_target
@@ -103,9 +105,6 @@ class UserMailer < ActionMailer::Base
 
     unless @event.book_id_usertoprepare.blank? #(ntype == "event_cancel_sender" || "event_set_sender") || 
       @book_usertoprepare = Book.find(@event.book_id_usertoprepare)
-
-      # host = Rails.env == 'production' ? 'www.casenexus.com' : 'localhost:3000'
-      # url = Rails.application.routes.url_helpers.root_url(host: host)
       attachments["case.pdf"] = File.read(File.join(Rails.root, 'app','assets','images','library',@book_usertoprepare.url))
     end
 
