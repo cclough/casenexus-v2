d37a61872902f8623aab86859f849cc5b7e78e60
Christian Clough <christian.clough@gmail.com>
2013-08-18 20:15:59 +0100

    Library tag filter work

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
     if !params[:btype].blank? && params[:btype] != "all"
       books_scope = Book.where(btype: params[:btype])
     else
-      books_scope = Book   
+      books_scope = Book 
     end
 
     if !params[:per_page].blank?
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
       books_per_page = 10
     end
 
-    @books = books_scope.search_for(params[:search]).order(sort_column + " " + sort_direction).paginate(per_page: books_per_page, page: params[:page])
+    @books = Book.search_for(params[:search]).order(sort_column + " " + sort_direction).paginate(per_page: books_per_page, page: params[:page])
 
 	end
 
@@ -43,6 +43,6 @@ private
   end
 
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
