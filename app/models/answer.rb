c9192e3038707d37b8be082207c9b55edf29c239
Christian Clough <christian.clough@gmail.com>
2013-08-19 04:01:12 +0100

    user profile questions answers and posts

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,16 +1,23 @@
 class Answer < ActiveRecord::Base
   attr_accessible :content, :question_id
 
-  # Associations
+  ### Associations
   belongs_to :question
   belongs_to :user
   has_many :comments, as: :commentable
 
-  # Validations
+  ### Validations
   validates_presence_of :question_id
   validates_presence_of :user_id
   validates :content, presence: true, length: { maximum: 1000 } 
 
-  # Voteable
+  ### Voteable
   acts_as_voteable
+
+  ### Micro
+
+  def content_trunc
+    content.truncate(17, separator: ' ')
+  end
+
 end
