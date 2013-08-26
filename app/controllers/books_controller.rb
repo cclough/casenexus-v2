ca020f21ebd4c0eaf9d45b2b9a1c45f1040e145b
Vincent Zhu <zhu1230@gmail.com>
2013-08-27 01:09:37 +0800

    fix book for tagging

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,8 +18,12 @@ class BooksController < ApplicationController
     end
     params[:tag_type_id] = nil if params[:tag_type_id] == [""]
     params[:tag_industry_id] = nil if params[:tag_industry_id] == [""]
-    
-    @books = Book.tagged_on(params[:tag_type_id] || Tag.where(category_id: 4).pluck(:id), params[:tag_industry_id] || Tag.where(category_id: 5).pluck(:id)).group('books.id').search_for(params[:search]).order(sort_column + " " + sort_direction).paginate(per_page: books_per_page, page: params[:page])
+    if params[:tag]
+      relation = Book.tagged_with(params[:tag])  
+    else
+      relation = Book.tagged_on(params[:tag_type_id] || Tag.where(category_id: 4).pluck(:id), params[:tag_industry_id] || Tag.where(category_id: 5).pluck(:id)).group('books.id')
+    end
+    @books = relation.search_for(params[:search]).order(sort_column + " " + sort_direction).paginate(per_page: books_per_page, page: params[:page])
 	end
 
   def show
