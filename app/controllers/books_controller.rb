0030a13ad2966d10ead5d8031a8c611d47c0dbdb
Christian Clough <christian.clough@gmail.com>
2013-09-02 22:54:53 -0700

    books problem

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,24 +5,28 @@ class BooksController < ApplicationController
   helper_method :sort_column, :sort_direction
 
 	def index
-    if !params[:btype].blank? && params[:btype] != "all"
-      books_scope = Book.where(btype: params[:btype])
-    else
-      books_scope = Book 
-    end
 
     if !params[:per_page].blank?
       books_per_page = params[:per_page]
     else
       books_per_page = 10
     end
+
     params[:tag_type_id] = nil if params[:tag_type_id] == [""]
     params[:tag_industry_id] = nil if params[:tag_industry_id] == [""]
+
     if params[:tag]
       relation = Book.tagged_with(params[:tag])  
-    else
+    elsif params[:tag_type_id] || params[:tag_industry_id]
       relation = Book.tagged_on(params[:tag_type_id] || Tag.where(category_id: 4).pluck(:id), params[:tag_industry_id] || Tag.where(category_id: 5).pluck(:id)).group('books.id')
+    else
+      if !params[:btype].blank? && params[:btype] != "all"
+        relation = Book.where(btype: params[:btype])
+      else
+        relation = Book
+      end
     end
+
     @books = relation.search_for(params[:search]).order(sort_column + " " + sort_direction).paginate(per_page: books_per_page, page: params[:page])
 	end
 
