da1b8f8124254cef050e071b610afa9677fe8315
Christian Clough <christian.clough@gmail.com>
2013-09-18 23:53:05 -0700

    profile empties

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,6 +6,7 @@ class ProfileController < ApplicationController
 
   def index
     @friends = current_user.accepted_friends
+
     @cases = current_user.cases
 
     @case_count_bracket = current_user.case_count_bracket
