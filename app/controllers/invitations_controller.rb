2d9f66301c5a9adf4baed6a058a1039e7f9ab62f
Christian Clough <christian.clough@gmail.com>
2013-09-29 17:26:46 -0700

    great redesign v3 - invitations now working

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -7,7 +7,7 @@ class InvitationsController < ApplicationController
     @invitations = Invitation.where(user_id: current_user.id)
     @invitation = current_user.invitations.build(params[:invitation])
     
-    render layout: false
+    render partial: "index", layout: false
 
   end
   def show
@@ -17,34 +17,23 @@ class InvitationsController < ApplicationController
   def create
     @invitation = current_user.invitations.build(params[:invitation])
 
-    if @invitation.save
-      flash[:notice] = "An invitation was sent to #{@invitation.email}"
-      if params[:back_url]
-        redirect_to params[:back_url]
+    respond_to do |format|
+      if @invitation.save
+        flash[:notice] = "An invitation was sent to #{@invitation.email}"
+        format.js
       else
-        redirect_to action: :index
-      end
-    else
-      flash[:error] = "An error was encountered while sending the invitation."
-      if params[:back_url]
-        @invitation.errors.messages.each do |key, value|
-          flash[:error] += "<br/>#{key}: #{value.join(", ")}.".html_safe
-        end
-        redirect_to params[:back_url]
-      else
-        render action: :new
+        @invitations = Invitation.where(user_id: current_user.id)
+        # @invitation = current_user.invitations.build(params[:invitation])
+        format.js
       end
     end
+
   end
 
   def destroy
     @invitation = current_user.invitations.find(params[:id])
     @invitation.destroy
     flash[:notice] = "Invitation cancelled"
-    if params[:back_url]
-      redirect_to params[:back_url]
-    else
-      redirect_to action: :index
-    end
+    redirect_to profile_path
   end
 end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -9,14 +9,13 @@ class Invitation < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-
-  validate :five_invitations_per_user
-
   validates_format_of :email,
                       :with => /(^[A-Z0-9._%-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$)|^$/i,
                       :message => "must be a valid email address"
+  validate :limit_invitations_per_user
+  validate :validate_university_email, on: :create
 
-  INVITATION_LIMIT = 5
+  INVITATION_LIMIT = 20
 
   def reset_activation_key
     begin
@@ -30,7 +29,7 @@ class Invitation < ActiveRecord::Base
 
   #private
 
-  def five_invitations_per_user
+  def limit_invitations_per_user
     if self.user_id && self.user_id != 1
       if Invitation.where(user_id: self.user_id).count >= INVITATION_LIMIT
         errors.add(:base, "max limit per user is 5 invitations")
@@ -43,4 +42,18 @@ class Invitation < ActiveRecord::Base
     charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
     (0...size).map{ charset.to_a[rand(charset.size)] }.join
   end
+
+  def validate_university_email
+    unless self.email == "christian.clough@gmail.com" || self.email == "cclough@candesic.com"
+      begin
+        # finds database listed domain within string after at sign http://codereview.stackexchange.com/questions/25814/ruby-check-if-email-address-contains-one-of-many-domains-from-a-table-ignoring/25836?noredirect=1#25836
+        domain = self.email.split("@")[1]
+        errors.add(:email, "not from a listed university") if University.all.none?{ |d| domain[d.domain] }
+      rescue Exception => e
+        errors.add(:email, "not from a listed university")
+      end
+    end
+  end
+
+
 end
