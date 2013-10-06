b26f6adb083aeec0fb6b3fb0b81e2da6edcdfcb8
Christian Clough <christian.clough@gmail.com>
2013-10-06 16:27:40 -0700

    path after sign up confirm popup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -17,7 +17,7 @@ class RegistrationsController < Devise::RegistrationsController
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
         expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+        respond_with resource, :location => '/'
       end
     else
       clean_up_passwords resource
