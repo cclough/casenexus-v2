c5655d22c61cb9f81af4bf9fd70ab66ce5a5d885
Christian Clough <christian.clough@gmail.com>
2013-10-06 23:17:54 -0700

    enable devise async - another hack trial

--- app/models/user.rb
+++ app/models/user.rb
@@ -295,11 +295,20 @@ class User < ActiveRecord::Base
       end
     end
 
-
-
-
   end
 
+
+
+  # devise email overides (to enable async)
+  def send_on_create_confirmation_instructions
+    Devise::Mailer.delay.confirmation_instructions(self)
+  end
+  def send_reset_password_instructions
+    Devise::Mailer.delay.reset_password_instructions(self)
+  end
+  def send_unlock_instructions
+    Devise::Mailer.delay.unlock_instructions(self)
+  end
 
   private
 
