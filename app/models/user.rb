49f62912cdddd6b109a791bc966f3b8c65fec1c7
Christian Clough <christian.clough@gmail.com>
2013-10-22 22:25:44 -0700

    MODELS CLEANING FINISHED

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable#, :token_authenticatable, :async ,:omniauthable
+         :confirmable
 
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me, :lat, :lng,
                   :skype, :email_admin, :email_users, :confirm_tac, :university, :university_id,
                   :invitation_code, :ip_address, :language_ids, :cases_external, :last_online_at, 
-                  :time_zone, :degree_level, :can_upvote, :can_downvote, :linkedin, :completed
+                  :time_zone, :degree_level, :linkedin, :completed
 
   attr_accessor :ip_address, :confirm_tac, :invitation_code
 
@@ -67,23 +67,21 @@ class User < ActiveRecord::Base
   validate :validate_invitation, on: :create
 
   # ON UPDATE
-  validates :username, presence: true, uniqueness: true, length: { maximum: 30 }, on: :update
   validates :lat, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }, on: :update
   validates :lng, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, on: :update
-  # validates :lng, :uniqueness => { :scope => :lat }
+  validate :validate_not_too_close_to_another?, on: :update
+
+  validates :username, presence: true, uniqueness: true, length: { maximum: 30 }, on: :update
   validates :linkedin, length: { maximum: 100 }
   validates :degree_level, presence: true, on: :update
   validates :skype, length: { maximum: 32 },
-            format: { with: /^[\W\w]+[a-z0-9\-]+$/i },
             allow_blank: true,
             on: :update
   validate :validate_has_languages?, on: :update
-  validate :validate_not_too_close_to_another?, on: :update
   validate :validate_cases_external?, on: :update
 
-
   ### Scopes
-  # For online user panel - vincent work
+  # unused now - vincent work
   scope :not_friends, ->(user) {select('users.*, friends.id as fid').joins("left join friendships on users.id = friendships.friend_id and friendships.user_id = #{User.sanitize(user.id)} left join users as friends on friends.id = friendships.friend_id").where("friends.id is null and users.id != ?",user.id)}
 
 
@@ -111,6 +109,7 @@ class User < ActiveRecord::Base
 
 
 
+  ### Micro
 
   def username_trunc
     (username).truncate(15, separator: ' ')
@@ -118,6 +117,10 @@ class User < ActiveRecord::Base
 
   def case_count_recd
     cases.count
+  end
+
+  def case_count_givn
+    Case.where("interviewer_id = ?", self.id).all.count
   end
 
   def case_count_external
@@ -128,23 +131,6 @@ class User < ActiveRecord::Base
     end
   end
 
-  def case_count_givn
-    Case.where("interviewer_id = ?", self.id).all.count
-  end
-
-  def case_count_bracket
-    # defines radar chart last 5 count
-    case case_count_recd
-    when 0
-      #signals to show 'you must do at least one case'
-      0
-    when 1..5
-      1
-    when 6..1000
-      5
-    end
-  end
-
   def degree_level_in_words
     case degree_level
     when 0
@@ -152,18 +138,6 @@ class User < ActiveRecord::Base
     else
       "MBA"
     end
-  end
-
-  def level
-    # case case_count_viewee
-    #   when 0..9 then 0
-    #   when 10..19 then 1
-    #   when 20..29 then 2
-    #   when 30..39 then 3
-    #   when 40..49 then 4
-    #   when 50..1000 then 5
-    # end.to_s
-    0
   end
 
   def online_now?
@@ -179,54 +153,6 @@ class User < ActiveRecord::Base
   end
 
   class << self
-
-    # def active
-    #   where(active: true)
-    # end
-
-    # def inactive
-    #   where(active: false)
-    # end
-
-    def markers(users)
-      users.map{ |m| { id: m.id, level: m.level, lat: m.lat, lng: m.lng } }
-    end
-
-    def markers_geojson(users)
-
-      # if users.count > 0
-        string = "["
-
-        for user in users
-          new_bit = "{
-                      \"type\": \"Feature\",
-                      \"geometry\": { \"type\": \"Point\", \"coordinates\": [#{user.lng},#{user.lat}] },
-                      \"properties\": {
-                        \"id\": \"#{user.id}\",
-                        \"username\": \"#{user.username_trunc}\",
-                        \"university_image\": \"#{user.university.image}\",
-                        \"university_name\": \"#{user.university.name.upcase}\",
-                        \"cases_recd\": \"#{user.case_count_recd}\",
-                        \"cases_givn\": \"#{user.case_count_givn}\",
-                        \"cases_ext\": \"#{user.cases_external}\",
-                        \"icon\": {
-                          \"iconUrl\": \"/assets/markers/marker_#{user.university.image}\",
-                          \"iconSize\": [35, 57],
-                          \"iconAnchor\": [17, 51]
-                        }
-                      }
-                    },"
-          string = string + new_bit
-        end
-
-        string = string.chop! + "]"
-        string
-
-      # end
-
-    end
-
-
 
     def confirmed
       where("confirmed_at is not null")
@@ -283,7 +209,6 @@ class User < ActiveRecord::Base
       completed.where("id <> ?", user.id)
     end
 
-    # Pulldown Filters
     def list_language(language_id)
       if !language_id.blank? && language_id != "0"
         completed.joins(:languages_users).where(languages_users: {language_id: language_id})
@@ -292,11 +217,40 @@ class User < ActiveRecord::Base
       end
     end
 
+    def markers(users)
+      users.map{ |m| { id: m.id, level: m.level, lat: m.lat, lng: m.lng } }
+    end
+
+    def markers_geojson(users)
+      string = "["
+      for user in users
+        new_bit = "{
+                    \"type\": \"Feature\",
+                    \"geometry\": { \"type\": \"Point\", \"coordinates\": [#{user.lng},#{user.lat}] },
+                    \"properties\": {
+                      \"id\": \"#{user.id}\",
+                      \"username\": \"#{user.username_trunc}\",
+                      \"university_image\": \"#{user.university.image}\",
+                      \"university_name\": \"#{user.university.name.upcase}\",
+                      \"cases_recd\": \"#{user.case_count_recd}\",
+                      \"cases_givn\": \"#{user.case_count_givn}\",
+                      \"cases_ext\": \"#{user.cases_external}\",
+                      \"icon\": {
+                        \"iconUrl\": \"/assets/markers/marker_#{user.university.image}\",
+                        \"iconSize\": [35, 57],
+                        \"iconAnchor\": [17, 51]
+                      }
+                    }
+                  },"
+        string = string + new_bit
+      end
+
+      string = string.chop! + "]"
+      string
+    end
   end
 
-
-
-  # devise email overides (to enable async)
+  # Devise email overides (to enable async)
   def send_on_create_confirmation_instructions
     Devise::Mailer.delay.confirmation_instructions(self)
   end
@@ -316,10 +270,10 @@ class User < ActiveRecord::Base
     else
       if Invitation.where(code: self.invitation_code).exists?
         if Invitation.where("code = ? and invited_id is not null", self.invitation_code).exists?
-          errors.add(:invitaiton_code, "already used")
+          errors.add(:invitaiton_code, "already used.")
         end
       else
-        errors.add(:invitation_code, "doesn't exist")
+        errors.add(:invitation_code, "doesn't exist.")
       end
     end
   end
@@ -356,7 +310,6 @@ class User < ActiveRecord::Base
     end
   end
 
-
   def update_invitation
     return if self.invitation_code == "BYPASS_CASENEXUS_INV"
     Invitation.where(code: self.invitation_code).first.update_attribute(:invited_id, self.id)
@@ -372,7 +325,7 @@ class User < ActiveRecord::Base
         errors.add(:base, "Sorry, casenexus is not yet available for your university")
       end
     else
-      self.university = University.find(1)
+      self.university = University.find(1) # Set to cambridge if on exception list
     end
 
   end
