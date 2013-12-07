15984dd8f74558cd747eaec1a0843215c72605f0
Christian Clough <christian.clough@gmail.com>
2013-12-07 23:33:37 +0000

    books_update_2 rake complete now

--- app/models/taggable.rb
+++ app/models/taggable.rb
@@ -19,8 +19,8 @@ module Taggable
     end
 
     def tag_list=(names)
-      names.pop #added pop and shift to remove first and last array items as chosen always including a blank field for unknown reason
-      names.shift
+      # names.pop #added pop and shift to remove first and last array items as chosen always including a blank field for unknown reason
+      # names.shift
       self.tags = names.map do |n| # here a split(", ") is removed to enable chosen multiple input
         Tag.where(name: n.strip).first_or_create!
       end
