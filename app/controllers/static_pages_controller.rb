14cbdac2cbf3cc646d4da9fa4bfc5536b13a85c6
Christian Clough <christian.clough@gmail.com>
2014-01-29 15:44:09 +0000

    fix terms and conditions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,32 +3,32 @@ class StaticPagesController < ApplicationController
   layout 'home'
 
   def home
-    if params[:code]
-      if params[:code] == "BYPASS_CASENEXUS_INV"
-        session[:code] = "BYPASS_CASENEXUS_INV"
-      elsif @invitation = Invitation.where(code: params[:code]).exists?
-        @invitation = Invitation.where(code: params[:code]).first
-        if @invitation.invited
-          session[:code] = nil
-          flash[:notice] = "Invitation already used"
-        else
-          flash[:notice] = "Hello #{@invitation.name}, thank you for accepting your invitation - complete the form to sign up for an account."
-          session[:code] = params[:code]
-        end
-      end
-    end
+    # if params[:code]
+    #   if params[:code] == "BYPASS_CASENEXUS_INV"
+    #     session[:code] = "BYPASS_CASENEXUS_INV"
+    #   elsif @invitation = Invitation.where(code: params[:code]).exists?
+    #     @invitation = Invitation.where(code: params[:code]).first
+    #     if @invitation.invited
+    #       session[:code] = nil
+    #       flash[:notice] = "Invitation already used"
+    #     else
+    #       flash[:notice] = "Hello #{@invitation.name}, thank you for accepting your invitation - complete the form to sign up for an account."
+    #       session[:code] = params[:code]
+    #     end
+    #   end
+    # end
     if signed_in?
-      flash[:notice] = "You must be signed out to accept an invitation" if @invitation
+      # flash[:notice] = "You must be signed out to accept an invitation" if @invitation
       redirect_to "/"
     else
       @login = User.new
       @user = User.new
-      @user.invitation_code = session[:code]
+      # @user.invitation_code = session[:code]
     end
   end
 
   def terms
-    @user = User.new unless signed_in?
+    # @user = User.new unless signed_in?
   end
 
 end
